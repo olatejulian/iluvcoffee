0c4519c838ebb79cec5b8dc83003db94f71f555d
Julian Olate <julian@brasilfone.com.br>
2022-05-04 15:19:44 -0300

    finished bonus mongoose chapter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeesController } from './coffees/coffees.controller';
-import { CoffeesService } from './coffees/coffees.service';
 import { CoffeesModule } from './coffees/coffee.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [CoffeesModule],
-    controllers: [AppController, CoffeesController],
-    providers: [AppService, CoffeesService],
+    imports: [
+        CoffeesModule,
+        MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -6,8 +6,9 @@ import {
     Param,
     Patch,
     Post,
-    // Query
+    Query,
 } from '@nestjs/common';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
@@ -16,15 +17,13 @@ export class CoffeesController {
     constructor(private readonly coffeesService: CoffeesService) {}
 
     @Get()
-    findAll() {
-        //   findAll(@Query() paginationQuery) {
-        // const { limit, offset } = paginationQuery;
-        return this.coffeesService.findAll();
+    findAll(@Query() paginationQuery: PaginationQueryDto) {
+        return this.coffeesService.findAll(paginationQuery);
     }
 
     @Get(':id')
-    findOne(@Param('id') id: number) {
-        return this.coffeesService.findOne('' + id);
+    findOne(@Param('id') id: string) {
+        return this.coffeesService.findOne(id);
     }
 
     @Post()
